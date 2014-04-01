c6eec722e9f6ab003896b3c601c4f45a77f612bb
Ben Mendis <dragonwisard@gmail.com>
2014-03-31 23:22:57 -0400

    Enable authentication
    Authentication requires a DB backend. Using MySQL which appears to be
    the default.

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,13 +4,27 @@
 apt-get update
 
 # Install Vim
-apt-get install vim
+apt-get -yq install vim
+
+# Install MySQL
+echo mysql-server-5.5 mysql-server/root_password password password | debconf-set-selections
+echo mysql-server-5.5 mysql-server/root_password_again password password| debconf-set-selections
+apt-get -yq install mysql-server-5.5
 
 # Install Kamailio
-apt-get install kamailio
+apt-get -yq install kamailio kamailio-mysql-modules
 
 # Copy Kamailio config into place
 cat /vagrant/kamailio > /etc/default/kamailio
+ln -sf /vagrant/kamailio.cfg /etc/kamailio/kamailio.cfg
+ln -sf /vagrant/kamctlrc /etc/kamailio/kamctlrc
+
+# Create the database
+kamdbctl create <<END
+password
+y
+y
+END
 
 # Start Kamailio
 /etc/init.d/kamailio start
